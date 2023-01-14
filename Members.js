a21bfbdb12ece1e4562db0309c4604a3f554a29a
oguzhanozturk3 <oguzhanozturk3@hotmail.com>
2023-01-15 01:01:47 +0300

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members =[
+    {
+        id:1,
+        name:'Jhon Doe',
+        email: 'jhon@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name:'Ali Veli',
+        email: 'ali@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id:3,
+        name:'Sam Tryn',
+        email: 'Sam@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members
